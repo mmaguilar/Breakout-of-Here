b425d00e156113d809cdf9140b750a4fe39bb9f4
Miriam Aguilar <m2002aguilar@gmail.com>
2022-10-05 18:38:10 -0700

    GameOver panel

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public int playerScore = 0;
     public TMP_Text displayText;
+    public TMP_Text gameOverText;
+    public GameObject gameOverPanel;
+    public TMP_Text gameOverPanelText;
+
 
     // Start is called before the first frame update
     void Start()
     {
         playerScore = 0;
         //displayText.text = "GOOOOO!!!";
+        gameOverPanel.SetActive(false);
+
     }
 
     // Update is called once per frame
@@ -30,6 +37,13 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         //Debug.Log("Game Over!!");
-        SceneManager.LoadScene(0);
+        StartCoroutine(gameOverSequence());
+    }
+
+    IEnumerator gameOverSequence(){
+        gameOverPanel.SetActive(true);
+        yield return new WaitForSeconds(3);
+                SceneManager.LoadScene(0);
+
     }
 }
